a1f1922c6ed089524648aa18ef8d525f51861721
DiepSigh <37123026+DiepSigh@users.noreply.github.com>
2018-03-21 19:53:05 -0400

    .

---
+++ Pokemon/Bulbasaur.cpp
@@ -0,0 +1,15 @@
+//Bulbasaur::Bulbasaur() {
+//	m_pokedex = 001;
+//	m_pokemon = "BULBASAUR";
+//
+//	m_baseHP = 45;
+//	m_baseAtk = 49;
+//	m_baseDef = 49;
+//	m_baseSpd = 45;
+//	m_baseSpe = 65;
+//
+//	m_type1 = GRASS;
+//	m_type2 = POISON;
+//	m_catchRate = 45;
+//	m_expYield = 64;
+//}
---
+++ Pokemon/Pokemon.h
@@ -0,0 +1,53 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+
+//Resources:
+//EXP Yield: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_effort_value_yield_(Generation_III-IV)
+//Data Structure: https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_data_structure_in_Generation_I
+//Base Stats: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)
+
+
+// Base of Pokémon
+#include <string>
+
+enum pokemon_type { NORMAL=0, FIGHTING=1, FLYING=2, POISON=3, GROUND=4, ROCK=5, BUG=7, GHOST=8, BLANK=9, FIRE=14, WATER=15, GRASS=16, ELECTRIC=17, PSYCHIC=18, ICE=19, DRAGON=20 };
+
+enum pokemon {MISSINGNO, BULBASAUR, IVYSAUR, VENUSAUR, CHARMANDER, CHARMELEON, CHARIZARD, SQUIRTLE, WARTORTLE, BLASTOISE, CATERPIE, METAPOD, BUTTERFREE,
+	  WEEDLE, KAKUNA, BEEDRILL, PIDGEY, PIDGEOTTO, PIDGEOT, RATTATA, RATICATE, SPEAROW, FEAROW, EKANS, ARBOK, PIKACHU, RAICHU, SANDSHREW, SANDSLASH,  
+	  NIDORAN_F, NIDORINA, NIDOQUEEN, NIDORAN_M, NIDORINO, NIDOKING, CLEFAIRY, CLEFABLE, VULPIX, NINETALES, JIGGLYPUFF, WIGGLYTUFF, ZUBAT, GOLBAT,  
+	  ODDISH, GLOOM, VILEPLUME, PARAS, PARASECT, VENONAT, VENOMOTH, DIGLETT, DUGTRIO};
+
+class Pokemon {
+public:
+
+	void setPokemon();
+	
+
+protected:
+	int m_pokedex; //stores # in pokedex
+	std::string m_name; //stores nickname if any
+
+	const int m_MAX_EV = 65535;
+
+	//dynamic stats
+	int m_hp;
+	int m_att;
+	int m_def;
+	int m_spd;
+	int m_spe;
+	int m_exp;
+
+	//static stats
+	int m_baseHP;
+	int m_baseAtk;
+	int m_baseDef;
+	int m_baseSpd;
+	int m_baseSpe;
+
+	int m_type1;
+	int m_type2;
+	int m_catchRate;
+	int m_expYield;
+};
+
+#endif // !POKEMON_H
