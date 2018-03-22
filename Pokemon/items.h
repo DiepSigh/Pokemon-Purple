bb3a1e3840235ad8e6e441620374df88d1b965f0
MikeMo <mikeemorra@gmail.com>
2018-03-22 13:27:16 -0400

    started again
    lame

---
+++ Pokemon/items.h
@@ -0,0 +1,21 @@
+#ifndef ITEMS_H
+#define ITEMS_H
+
+
+#include <string>
+using namespace std;
+
+
+class Items {
+public:
+	Items();
+	Items(std::string);
+	//GETTERS
+	string GetItemName() { return itemName; }
+	//SETTERS
+	void SetItemNAme(std::string name) { itemName = name; }
+private:
+	std::string itemName;
+};
+
+#endif // !ITEMS_H
