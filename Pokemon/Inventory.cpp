28ec281b8d11b5be510a1722c9f005390ee13a40
Mike <mikeemorra@gmail.com>
2018-03-23 10:47:21 -0700

    STARTED INVENTORY

--- Pokemon/Inventory.cpp
+++ Pokemon/Inventory.cpp
@@ -1,1 +1,7 @@
 #include "inventory.h"
+
+Inventory::Inventory() {}
+
+Inventory::Inventory(int capacity) {
+	m_capacity = capacity;
+}
--- Pokemon/Inventory.h
+++ Pokemon/Inventory.h
@@ -1,5 +1,18 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
+class Inventory {
+public:
+	Inventory();
+	Inventory(int);
+	void RemoveEffect();
+
+	//GETTERS
+
+	//SETTERS
+
+private:
+	int m_capacity;
+
 
 #endif
