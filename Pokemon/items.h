1c44857c0e2618cb0b2c3efbf72dcc21958dea5d
Mike <mikeemorra@gmail.com>
2018-03-23 07:58:40 -0700

    Added base item class
     and all derived classes

--- Pokemon/items.h
+++ Pokemon/items.h
@@ -1,21 +1,77 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
-
 #include <string>
-using namespace std;
 
-
-class Items {
+class Item {
 public:
-	Items();
-	Items(std::string);
+	Item();
+	Item( std::string, int, int, std::string );
+
 	//GETTERS
-	string GetItemName() { return itemName; }
+	std::string GetItemName() { return m_itemName; }
+	int GetId() { return m_itemId; }
+	int GetCost() { return m_itemCost; }
+	std::string GetItemDescription() { return m_itemDescription; }
+
 	//SETTERS
-	void SetItemNAme(std::string name) { itemName = name; }
+	void SetItemName(std::string name) { m_itemName = name; }
+	void SetItemId(int id) { m_itemId = id; }
+	void SetItemCost(int cost) { m_itemCost = cost; }
+	void SetItemDescription(std::string description) { m_itemDescription = description; }
+
 private:
-	std::string itemName;
+	std::string m_itemName;
+	int m_itemId;
+	int m_itemCost;
+	std::string m_itemDescription;
+};
+
+class Potion : public Item
+{
+public:
+	std::string m_itemName = "Potion";
+	int m_itemId = 01;
+	int m_itemCost = 300;
+	std::string m_itemDescription = "Heals 20 HP to a Pokémon./n";
+
+};
+
+class Antidote : public Item
+{
+public:
+	std::string m_itemName = "Antidote";
+	int m_itemId = 02;
+	int m_itemCost = 100;
+	std::string m_itemDescription = "Heals poison effects from a Pokémon./n";
+
+};
+
+class PokeBall : public Item
+{
+public:
+	std::string m_itemName = "PokeBall";
+	int m_itemId = 03;
+	int m_itemCost = 200;
+	std::string m_itemDescription = "A capsule used for catching Pokémon with a standard catch rate./n";
+};
+
+class ParalyzHeal : public Item
+{
+public:
+	std::string m_itemName = "ParalyzHeal";
+	int m_itemId = 04;
+	int m_itemCost = 200;
+	std::string m_itemDescription = "Heals paralysis effects from a Pokémon./n";
+};
+
+class BurnHeal : public Item
+{
+public:
+	std::string m_itemName = "BurnHeal";
+	int m_itemId = 05;
+	int m_itemCost = 250;
+	std::string m_itemDescription = "Heals burn effects from a Pokémon./n";
 };
 
 #endif // !ITEMS_H
