2c3bc0fe887200c51bb17ac0dc8dc3ad2ac484ef
MikeMo <mikeemorra@gmail.com>
2018-03-23 14:23:57 -0400

    mike

--- Pokemon/items.h
+++ Pokemon/items.h
@@ -1,23 +1,32 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
-
 #include <string>
-using namespace std;
 
 
 class Items {
 public:
 
 	Items();
-	Items(std::string);
+	Items(std::string, int, int, std::string);
 	//GETTERS
-	string GetItemName() { return itemName; }
+	std::string GetName() { return itemName; }
+	int GetId() { return itemId; }
+	int GetCost() { return itemCost; }
+	std::string GetItemDescription() { return itemDescription; }
+
 	//SETTERS
-	void SetItemNAme(std::string name) { itemName = name; }
+	void SetName(std::string name) { itemName = name; }
+	void SetId(int id) { itemId = id; }
+	void SetCost(int cost) { itemCost = cost; }
+	void SetDescription(std::string description) { itemDescription = description; }
 
 private:
 	std::string itemName;
+	int itemId;
+	int itemCost;
+	std::string itemDescription;
+
 };
 
 #endif // !ITEMS_H
