814ecd0b4a7006c577ea0b3433a066ddf9e1154c
MikeMo <mikeemorra@gmail.com>
2018-03-26 13:05:53 -0400

    cam

--- Pokemon/Camera.cpp
+++ Pokemon/Camera.cpp
@@ -1,1 +1,10 @@
-include "camera.h"
+#include "camera.h"
+#include "gameManager.h"
+
+SDL_Rect Camera::cameraRect = { 0, 0, 800, 640 };
+
+Camera::Camera() {}
+
+Camera::Camera(SDL_Rect) {
+
+}	
--- Pokemon/camera.h
+++ Pokemon/camera.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <string>
+
+class Camera {
+	Camera();
+	Camera(SDL_Rect);	
+
+public:
+	static SDL_Rect cameraRect;
+	
+private:
+	
+}
+
+
+#endif
