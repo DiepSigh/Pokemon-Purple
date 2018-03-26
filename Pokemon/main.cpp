95f5e6b99fde39fb9e0f100180de225b32eeea6c
DiepSigh <37123026+DiepSigh@users.noreply.github.com>
2018-03-26 14:47:18 -0400

    Merge complete?

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,14 +1,19 @@
 // Pokemon.cpp : Defines the entry point for the console application.
 //
 #include "gameManager.h"
-
+#include "Pokemon.h"
+#include <time.h>
 int main(int argc, char *argv[])
 {
 	GameManager* game = GameManager::Instance();
 	game->Run();
 	GameManager::Release();
 	game = nullptr;
-	
+
+	srand(static_cast<unsigned int> (time(0))); //seed
+	Pokemon Encounter(SQUIRTLE, 7);
+
+	Encounter.displayStats();
     return 0;
 }
 
