bf0f01eaaef1b4d6adad57ea4043e8bf7f744120
MikeMo <mikeemorra@gmail.com>
2018-03-26 15:36:49 -0400

    commiting

--- Pokemon/camera.cpp
+++ Pokemon/camera.cpp
@@ -1,11 +1,12 @@
 #include "camera.h"
-#include "gameManager.h"
+	
+
 
 
 Camera::Camera() {}
 
 Camera::Camera(SDL_Rect, int screenWidth,int screenHeight) {
-	screenWidth = 800;
-	screenHeight = 640;
+
 }
-	SDL_Rect Camera::cameraRect = { 0, 0,screenWidth, screenHeight };
+
+SDL_Rect Camera::cameraRect = { 0, 0, Graphics::SCREEN_WIDTH, Graphics::SCREEN_HEIGHT };
--- Pokemon/camera.h
+++ Pokemon/camera.h
@@ -1,14 +1,17 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "SDL_Rect.h"
+#include "graphics.h"
+
 class Camera {
 	Camera();
 	Camera(SDL_Rect, int, int);	
 
 public:
 	static SDL_Rect cameraRect;
-	static int screenWidth;
-	static int screenHeight;
+	int screenWidth = Graphics::SCREEN_WIDTH;
+	int screenHeight = Graphics::SCREEN_WIDTH;
 private:
 	
 };
