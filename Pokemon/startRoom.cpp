5baf88c8cccb75293fc00cd2555dde2004745efc
Canados1985 <37122977+Canados1985@users.noreply.github.com>
2018-03-26 22:02:52 -0400

    Monday_home

--- Pokemon/startRoom.cpp
+++ Pokemon/startRoom.cpp
@@ -2,18 +2,167 @@
 
 StartRoom::StartRoom() {
 
-	mFloor = new AnimatedTexture("01_Start_Game_Room.png", 0, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
-	mObjects = new AnimatedTexture("01_Start_Game_Room.png", 16, 16, 32, 32, 1, 0, AnimatedTexture::HORIZONTAL);
-	mComputer = new AnimatedTexture("01_Start_Game_Room.png", 0, 16, 32, 32, 1, 0, AnimatedTexture::HORIZONTAL);
+	mComputer = new AnimatedTexture("01_Start_Game_Room.png", 0, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall = new AnimatedTexture("01_Start_Game_Room.png", 16, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall2 = new AnimatedTexture("01_Start_Game_Room.png", 32, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall3 = new AnimatedTexture("01_Start_Game_Room.png", 48, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall4 = new AnimatedTexture("01_Start_Game_Room.png", 64, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall5 = new AnimatedTexture("01_Start_Game_Room.png", 80, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall6 = new AnimatedTexture("01_Start_Game_Room.png", 96, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mWall7 = new AnimatedTexture("01_Start_Game_Room.png", 112, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mComputer2 = new AnimatedTexture("01_Start_Game_Room.png", 0, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mTable = new AnimatedTexture("01_Start_Game_Room.png", 16, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mTable2 = new AnimatedTexture("01_Start_Game_Room.png", 32, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor = new AnimatedTexture("01_Start_Game_Room.png", 48, 0, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor2 = new AnimatedTexture("01_Start_Game_Room.png", 64, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor3 = new AnimatedTexture("01_Start_Game_Room.png", 80, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor4 = new AnimatedTexture("01_Start_Game_Room.png", 96, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mStairs = new AnimatedTexture("01_Start_Game_Room.png", 112, 16, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mFloor5 = new AnimatedTexture("01_Start_Game_Room.png", 0, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor6 = new AnimatedTexture("01_Start_Game_Room.png", 16, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor7 = new AnimatedTexture("01_Start_Game_Room.png", 32, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor8 = new AnimatedTexture("01_Start_Game_Room.png", 48, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor9 = new AnimatedTexture("01_Start_Game_Room.png", 64, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor10 = new AnimatedTexture("01_Start_Game_Room.png", 80, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor11 = new AnimatedTexture("01_Start_Game_Room.png", 96, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor12 = new AnimatedTexture("01_Start_Game_Room.png", 112, 32, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mFloor13 = new AnimatedTexture("01_Start_Game_Room.png", 0, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor14 = new AnimatedTexture("01_Start_Game_Room.png", 16, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor15 = new AnimatedTexture("01_Start_Game_Room.png", 32, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor16 = new AnimatedTexture("01_Start_Game_Room.png", 48, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor17 = new AnimatedTexture("01_Start_Game_Room.png", 64, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor18 = new AnimatedTexture("01_Start_Game_Room.png", 80, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor19 = new AnimatedTexture("01_Start_Game_Room.png", 96, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor20 = new AnimatedTexture("01_Start_Game_Room.png", 112, 48, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mFloor21 = new AnimatedTexture("01_Start_Game_Room.png", 0, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor22 = new AnimatedTexture("01_Start_Game_Room.png", 16, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor23 = new AnimatedTexture("01_Start_Game_Room.png", 32, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mTelevision = new AnimatedTexture("01_Start_Game_Room.png", 48, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor24 = new AnimatedTexture("01_Start_Game_Room.png", 64, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor25 = new AnimatedTexture("01_Start_Game_Room.png", 80, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor26 = new AnimatedTexture("01_Start_Game_Room.png", 96, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor27 = new AnimatedTexture("01_Start_Game_Room.png", 112, 64, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mFloor28 = new AnimatedTexture("01_Start_Game_Room.png", 0, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor29 = new AnimatedTexture("01_Start_Game_Room.png", 16, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor30 = new AnimatedTexture("01_Start_Game_Room.png", 32, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mConsole = new AnimatedTexture("01_Start_Game_Room.png", 48, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor31 = new AnimatedTexture("01_Start_Game_Room.png", 64, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor32 = new AnimatedTexture("01_Start_Game_Room.png", 80, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor33 = new AnimatedTexture("01_Start_Game_Room.png", 96, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor34 = new AnimatedTexture("01_Start_Game_Room.png", 112, 80, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mBed = new AnimatedTexture("01_Start_Game_Room.png", 0, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor35 = new AnimatedTexture("01_Start_Game_Room.png", 16, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor36 = new AnimatedTexture("01_Start_Game_Room.png", 32, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor37 = new AnimatedTexture("01_Start_Game_Room.png", 48, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor38 = new AnimatedTexture("01_Start_Game_Room.png", 64, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor39 = new AnimatedTexture("01_Start_Game_Room.png", 80, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFlower = new AnimatedTexture("01_Start_Game_Room.png", 96, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor40 = new AnimatedTexture("01_Start_Game_Room.png", 112, 96, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
+	mBed2 = new AnimatedTexture("01_Start_Game_Room.png", 0, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor41 = new AnimatedTexture("01_Start_Game_Room.png", 16, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor42 = new AnimatedTexture("01_Start_Game_Room.png", 32, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor43 = new AnimatedTexture("01_Start_Game_Room.png", 48, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor44 = new AnimatedTexture("01_Start_Game_Room.png", 64, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor45 = new AnimatedTexture("01_Start_Game_Room.png", 80, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFlower2 = new AnimatedTexture("01_Start_Game_Room.png", 96, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+	mFloor46 = new AnimatedTexture("01_Start_Game_Room.png", 112, 112, 16, 16, 1, 0, AnimatedTexture::HORIZONTAL);
+
 }
 
 StartRoom::~StartRoom() {
-	delete mFloor;
-	mFloor = NULL;
-
-	delete mObjects;
-	mObjects = NULL;
-
 	delete mComputer;
 	mComputer = NULL;
+	delete mWall;
+	mWall = NULL;
+	delete mWall2;
+	mWall2 = NULL;
+	delete mWall3;
+	mWall3 = NULL;
+	delete mWall4;
+	mWall4 = NULL;
+	delete mWall5;
+	mWall5 = NULL;
+	delete mWall6;
+	mWall6 = NULL;
+	delete mWall7;
+	mWall7 = NULL;
+
+
+	delete mComputer2;
+	mComputer2 = NULL;
+	delete mTable;
+	mTable = NULL;
+	delete mTable2;
+	mTable2 = NULL;
+	delete mFloor;
+	mFloor = NULL;
+	delete mFloor2;
+	mFloor2 = NULL;
+	delete mFloor3;
+	mFloor3 = NULL;
+	delete mFloor4;
+	mFloor4 = NULL;
+	delete mStairs;
+	mStairs = NULL;
+
+
+	delete mFloor5;
+	mFloor5 = NULL;
+	delete mFloor6;
+	mFloor6 = NULL;
+	delete mFloor7;
+	mFloor7 = NULL;
+	delete mFloor8;
+	mFloor8 = NULL;
+	delete mFloor9;
+	mFloor9 = NULL;
+	delete mFloor10;
+	mFloor10 = NULL;
+	delete mFloor11;
+	mFloor11 = NULL;
+	delete mFloor12;
+	mFloor12 = NULL;
+
+
+	delete mFloor13;
+	mFloor13 = NULL;
+	delete mFloor14;
+	mFloor14 = NULL;
+	delete mFloor15;
+	mFloor15 = NULL;
+	delete mFloor16;
+	mFloor16 = NULL;
+	delete mFloor17;
+	mFloor17 = NULL;
+	delete mFloor18;
+	mFloor18 = NULL;
+	delete mFloor19;
+	mFloor18 = NULL;
+	delete mFloor20;
+	mFloor20 = NULL;
+
+	delete mFloor21;
+	mFloor21 = NULL;
+	delete mFloor22;
+	mFloor22 = NULL;
+	delete mFloor23;
+	mFloor23 = NULL;
+	delete mTelevision;
+	mTelevision = NULL;
+	delete mFloor24;
+	mFloor24 = NULL;
+	delete mFloor25;
+	mFloor25 = NULL;
+	delete mFloor26;
+	mFloor26 = NULL;
+	delete mFloor27;
+	mFloor27 = NULL;
+
 }
--- Pokemon/startRoom.h
+++ Pokemon/startRoom.h
@@ -6,16 +6,113 @@
 class StartRoom : public AnimatedTexture {
 
 protected:
+
+	//AnimatedTexture* tileSet[64] = { mWall, mWall2, mWall3, mWall4, mWall5, mWall6, mWall7 };
+	
+	AnimatedTexture* mWall;
+	AnimatedTexture* mWall2;
+	AnimatedTexture* mWall3;
+	AnimatedTexture* mWall4;
+	AnimatedTexture* mWall5;
+	AnimatedTexture* mWall6;
+	AnimatedTexture* mWall7;
+
+	AnimatedTexture* mStairs;
+	AnimatedTexture* mTable;
+	AnimatedTexture* mTable2;
+	AnimatedTexture* mTelevision;
+	AnimatedTexture* mConsole;
+
 	AnimatedTexture* mFloor;
+	AnimatedTexture* mFloor2;
+	AnimatedTexture* mFloor3;
+	AnimatedTexture* mFloor4;
+	AnimatedTexture* mFloor5;
+	AnimatedTexture* mFloor6;
+	AnimatedTexture* mFloor7;
+	AnimatedTexture* mFloor8;
+	AnimatedTexture* mFloor9;
+	AnimatedTexture* mFloor10;
+	AnimatedTexture* mFloor11;
+	AnimatedTexture* mFloor12;
+	AnimatedTexture* mFloor13;
+	AnimatedTexture* mFloor14;
+	AnimatedTexture* mFloor15;
+	AnimatedTexture* mFloor16;
+	AnimatedTexture* mFloor17;
+	AnimatedTexture* mFloor18;
+	AnimatedTexture* mFloor19;
+	AnimatedTexture* mFloor20;
+	AnimatedTexture* mFloor21;
+	AnimatedTexture* mFloor22;
+	AnimatedTexture* mFloor23;
+	AnimatedTexture* mFloor24;
+	AnimatedTexture* mFloor25;
+	AnimatedTexture* mFloor26;
+	AnimatedTexture* mFloor27;
+	AnimatedTexture* mFloor28;
+	AnimatedTexture* mFloor29;
+	AnimatedTexture* mFloor30;
+	AnimatedTexture* mFloor31;
+	AnimatedTexture* mFloor32;
+	AnimatedTexture* mFloor33;
+	AnimatedTexture* mFloor34;
+	AnimatedTexture* mFloor35;
+	AnimatedTexture* mFloor36;
+	AnimatedTexture* mFloor37;
+	AnimatedTexture* mFloor38;
+	AnimatedTexture* mFloor39;
+	AnimatedTexture* mFloor40;
+	AnimatedTexture* mFloor41;
+	AnimatedTexture* mFloor42;
+	AnimatedTexture* mFloor43;
+	AnimatedTexture* mFloor44;
+	AnimatedTexture* mFloor45;
+	AnimatedTexture* mFloor46;
+
+
 	AnimatedTexture* mObjects;
 	AnimatedTexture* mComputer;
+	AnimatedTexture* mComputer2;
+	AnimatedTexture* mBed;
+	AnimatedTexture* mBed2;
+	AnimatedTexture* mFlower;
+	AnimatedTexture* mFlower2;
+
 
 public:
 	StartRoom();
 	~StartRoom();
-	inline AnimatedTexture* GetFloor() {return mFloor;}
-	inline AnimatedTexture* GetObjects() {return mObjects;}
-	inline AnimatedTexture* GetComputer() {return mComputer;}
+
+	inline AnimatedTexture* GetWall() { return mWall; }
+	inline AnimatedTexture* GetWall2() { return mWall2; }
+	inline AnimatedTexture* GetWall3() { return mWall3; }
+	inline AnimatedTexture* GetWall4() { return mWall4; }
+	inline AnimatedTexture* GetWall5() { return mWall5; }
+	inline AnimatedTexture* GetWall6() { return mWall6; }
+	inline AnimatedTexture* GetWall7() { return mWall7; }
+
+
+	inline AnimatedTexture* GetStairs() { return mStairs; }
+
+	inline AnimatedTexture* GetTable() { return mTable; }
+	inline AnimatedTexture* GetTable2() { return mTable2; }
+
+	inline AnimatedTexture* GetTelevision() { return mTelevision; }
+	inline AnimatedTexture* GetConsole() { return mConsole; }
+
+	inline AnimatedTexture* GetFloor() { return mFloor; }
+
+
+	inline AnimatedTexture* GetComputer() { return mComputer; }
+	inline AnimatedTexture* GetComputer2() { return mComputer2; }
+
+	inline AnimatedTexture* GetBed() { return mBed; }
+	inline AnimatedTexture* GetBed2() { return mBed2; }
+
+	inline AnimatedTexture* GetFlower() { return mFlower; }
+	inline AnimatedTexture* GetFlower2() { return mFlower2; }
+
 
 };
 
