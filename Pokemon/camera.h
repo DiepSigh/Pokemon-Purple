83d117ccb86fd127a247ddd06750fecdbca66693
AlmightyMike <mikeemorra@gmail.com>
2018-04-05 13:32:01 -0400

    cleared cam scripts

--- Pokemon/camera.h
+++ Pokemon/camera.h
@@ -4,46 +4,6 @@
 #include "characters.h"
 #include "graphics.h"
 
-class Camera : public GameEntity{
-
-private:
-	static Camera* sInstance;
-	static bool sInitialized;
-
-public:
-	Camera(int,int);
-	Camera();
-	~Camera();
-
-	static Camera* Instance();
-	static bool Initialized();
-	void UpdatePos(Characters &mPlayer);
-	void Render();
-	//GETTERS
-	float GetPlayerXPos() { return mPlayerXPos; }
-	float GetPlayerYPos() { return mPlayerYPos; }
-	int GetCamXPos() { return mCamXPos; }
-	int GetCamYPos() { return mCamYPos; }
-	//Camera* GetCamXPos;
-	//Camera* GetCamXPos() { return mCamXPos; }
-
-
-	//SETTERS
-	 void SetPlayerXPos(int playerx) { mPlayerXPos = playerx; }
-	 void SetPlayerYPos(int playery) { mPlayerYPos = playery; }
-	 void SetCamXPos(float camx) { mPlayerXPos = camx; }
-	 void SetCamYPos(float camy) { mPlayerYPos = camy; }
-	 static SDL_Rect mMainCamera;
-
-private:
-	
-	float mPlayerXPos;
-	float mPlayerYPos;	
-	float mCamXPos;
-	float mCamYPos;
-	Graphics* mGraphics;
-};
-
 
 #endif // !CAMERA_H
 
